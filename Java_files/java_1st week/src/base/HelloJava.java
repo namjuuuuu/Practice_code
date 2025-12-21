0ac0f98c2fd100e0c2033edaec0b7548e96bd834
namjuuuuu <knju2002g@gmail.com>
2025-12-21 18:32:40 +0900

    ~var3

--- Java_files/java_1st week/src/base/HelloJava.java
+++ Java_files/java_1st week/src/base/HelloJava.java
@@ -1,3 +1,5 @@
+package base;
+
 public class HelloJava {
     public static void main(String[] args) {
         System.out.println("Hello java");
---
+++ Java_files/java_2nd week/src/variable/Var1.java
@@ -0,0 +1,12 @@
+package variable;
+
+public class Var1 {
+    public static void main(String[] args){
+        System.out.println(10);
+        System.out.println(10);
+        System.out.println(10);
+    }
+}
+
+//숫자 10을 세 번 출력하는 게 아니라 20을 세 번 출력하는 코드로 바꾸고 싶을 때,
+//Var2 참고
