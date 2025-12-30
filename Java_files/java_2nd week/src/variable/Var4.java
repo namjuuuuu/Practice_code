dd05e1a041c62f25b98967716dbc40b122cbb8bf
namjuuuuu <knju2002g@gmail.com>
2025-12-30 19:36:42 +0900

    음..

---
+++ Java_files/java_2nd week/src/variable/Var4.java
@@ -0,0 +1,10 @@
+package variable;
+
+public class Var4 {
+    public void main(String[] args){
+        int a;
+        int b;
+
+        int c, d; //한 번에 여러 변수 선언 가능.
+    }
+}
---
+++ Java_files/java_2nd week/src/variable/Var6.java
@@ -0,0 +1,10 @@
+package variable;
+
+public class Var6 {
+    public static void main(String[] args) {
+        //변수를 초기화하지 않는다면?
+        int a;
+        // System.out.println(a); //java: variable a might not have been initialized
+                                  //(주석을 풀면) 실행 안됨.
+    }
+}
